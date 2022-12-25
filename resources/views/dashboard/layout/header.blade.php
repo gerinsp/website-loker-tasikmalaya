aa1b09795ef29b26b1e14c9ee9d64ea22df3f302
gerinsp <denapratama7@gmail.com>
2022-12-25 22:33:31 +0700

    fix bug turbolink

--- resources/views/dashboard/layout/header.blade.php
+++ resources/views/dashboard/layout/header.blade.php
@@ -1,5 +1,5 @@
 <header class="navbar navbar-dark sticky-top flex-md-nowrap p-0 shadow" style="background-color: rgb(45 62 80)">
-    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="/">LokerTasikmalaya</a>
+    <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" data-turbolinks="false" href="/">LokerTasikmalaya</a>
     <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
